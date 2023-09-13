81e57fb4100a563cce42401cbfd802a2b90fab3d
KimJoonSeo <junjun901225@gmail.com>
2023-09-13 23:17:18 +0900

    Feat: "추가 Post List API"
    PostSerializer 추가
    PostList API 추가

---
+++ post/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from post import views
+
+urlpatterns = [
+    path('', views.PostList.as_view()),
+]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from post.models import Post
+from post.serializers import PostSerializer
+
 
 # Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
