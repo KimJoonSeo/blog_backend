41e427d2d8a5cfc4fe1eec49299ce37dc8ff450a
KimJoonSeo <junjun901225@gmail.com>
2023-09-14 22:07:46 +0900

    Feat: "추가 Post Serializer 클래스들"
    Post 관련 모델들을 serialize, deserialize 하기 위한 클래스 생성

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework import serializers
 
-from post.models import Post
+from post.models import Post, PostTag, Comment
+
+
+class PostTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostTag
+        fields = ['tags']
 
 
 class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    post_tags = serializers.SerializerMethodField()
+
+    def get_post_tags(self, post):
+        post_tags_qs = PostTag.objects.filter(post=post)
+        return PostTagSerializer(post_tags_qs, many=True).data
+
     class Meta:
         model = Post
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
         fields = '__all__'
