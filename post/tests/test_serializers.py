529cbd5390879d16c063d49156e25d9dc55410bb
KimJoonSeo <junjun901225@gmail.com>
2023-09-16 12:47:21 +0900

    Test: "추가 Post에 달린 Comment 개수 테스트"
    - Post에 달린 Comment 테스트케이스 추가

--- post/tests/test_serializers.py
+++ post/tests/test_serializers.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from post.models import Tag, Post, PostTag
+from post.models import Tag, Post, PostTag, Comment
 from post.serializers import PostSerializer
 
 
-class MyTestCase(TestCase):
+class PostSerializerTestClass(TestCase):
     post = None
+    serializer = None
 
     @classmethod
     def setUpTestData(cls):
@@ -13,14 +14,22 @@ class MyTestCase(TestCase):
         tag1 = Tag.objects.create(name='tag1')
         tag2 = Tag.objects.create(name='tag2')
         tag3 = Tag.objects.create(name='tag3')
+
         cls.post = Post.objects.create(title='title',
                                        contents='contents',
                                        owner=user)
+
         PostTag.objects.create(post=cls.post, tag=tag1)
         PostTag.objects.create(post=cls.post, tag=tag2)
         PostTag.objects.create(post=cls.post, tag=tag3)
 
-    def test_total_cnt_for_post_tag(self):
-        serializer = PostSerializer(instance=self.post)
-        print(serializer.get_post_tags(self.post))
-        self.assertEqual(len(serializer.get_post_tags(self.post)), 3)
+        Comment.objects.create(owner=user, post=cls.post, contents='comment1')
+        Comment.objects.create(owner=user, post=cls.post, contents='comment2')
+
+        cls.serializer = PostSerializer(instance=cls.post)
+
+    def test_total_cnt_of_post_tags(self):
+        self.assertEqual(len(self.serializer.get_post_tags(self.post)), 3)
+
+    def test_total_cnt_of_comments(self):
+        self.assertEqual(self.serializer.get_cnt_of_comments(self.post), 2)
